f8a12cb20538b02de72e84755e356b1ba715687b
SXV357 <shrevis2018@gmail.com>
2023-05-10 22:10:24 -0500

    initial commit

---
+++ NBodySimulation/NBodySimulation.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NBodySimulation
+{
+	private Body[] bodies;    //stores all the bodies in the simulation
+	private int    numBodies; //number of bodies in this simulation
+	private double uRadius;   //radius of the universe
+	private String fileName;  //file providing the input
+
+	public NBodySimulation(String nameOfFile)
+	{
+		//TODO
+
+		initCanvas(); //don't move, should be the last line of the constructor
+	}
+
+	/** initialize the drawing canvas */
+	private void initCanvas()
+	{
+		StdDraw.setScale(-uRadius, uRadius); //set canvas size / scale
+		StdDraw.picture(0, 0, "starfield.jpg"); //paint background
+
+		//below is a for-each loop, which we will cover in the next lab
+		//more info is available in the powerpoints, for the curious
+		for (Body body : bodies)
+			body.draw();
+	}
+
+	/**
+	 * run the n-bodies simulation
+	 * @param T total time to run the simulation, in seconds
+	 * @param dt time step, in seconds
+	 */
+	public void run(double T, double dt)
+	{
+		//TODO
+	}
+}
