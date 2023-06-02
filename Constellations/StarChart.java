9443ca67d5421ade686777dec0b53d196a19e04e
SXV357 <shrevis2018@gmail.com>
2023-06-02 16:25:59 -0500

    draw all constellations

--- Constellations/StarChart.java
+++ Constellations/StarChart.java
@@ -1,87 +1,148 @@
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.*;
-import java.awt.Color;
+import java.util.ArrayList;
 
 public class StarChart {
-    private ArrayList<Star> stars;
-    private HashMap<Integer, Point> identityToLocation;
-    private HashMap<ArrayList<String>, Integer> namesToIdentity;
 
-    public StarChart(String fileName) throws FileNotFoundException{
-        this.stars = new ArrayList<Star>();
-        this.identityToLocation = new HashMap<Integer, Point>();
-        this.namesToIdentity = new HashMap<ArrayList<String>, Integer>();
-        Scanner file = new Scanner(new File(fileName));
-        while (file.hasNextLine()){
-            String[] arr = file.nextLine().split(" ");
-            double xCord = Double.parseDouble(arr[0]);
-            double yCord = Double.parseDouble(arr[1]);
-            int henryDraper = Integer.parseInt(arr[3]);
-            double magnitude = Double.parseDouble(arr[4]);
-            ArrayList<String> starNames = new ArrayList<String>();
-            if (arr.length >= 7){
-                String[] modifiedArr = Arrays.copyOfRange(arr, 6, arr.length);
-                for (int i = 0; i < modifiedArr.length; i++){
-                    modifiedArr[i] = modifiedArr[i].replace(";", "");
+  private ArrayList<Star> stars;
+  private HashMap<Integer, Point> identityToLocation;
+  private HashMap<ArrayList<String>, Integer> namesToIdentity;
+  static final int SIZE = 512;
+
+  public StarChart(String fileName) throws FileNotFoundException {
+    StdDraw.setCanvasSize(SIZE, SIZE);
+    StdDraw.setXscale(0, SIZE);
+    StdDraw.setYscale(0, SIZE);
+    this.stars = new ArrayList<Star>();
+    this.identityToLocation = new HashMap<Integer, Point>();
+    this.namesToIdentity = new HashMap<ArrayList<String>, Integer>();
+    Scanner file = new Scanner(new File(fileName));
+    while (file.hasNextLine()) {
+      String[] arr = file.nextLine().split(" ");
+      double xCord = Double.parseDouble(arr[0]);
+      double yCord = Double.parseDouble(arr[1]);
+      int henryDraper = Integer.parseInt(arr[3]);
+      double magnitude = Double.parseDouble(arr[4]);
+      ArrayList<String> starNames = new ArrayList<String>();
+      if (arr.length >= 7) {
+        String[] modifiedArr = Arrays.copyOfRange(arr, 6, arr.length);
+        for (int i = 0; i < modifiedArr.length; i++) {
+          modifiedArr[i] = modifiedArr[i].replace(";", "");
+        }
+        for (int j = 0; j < modifiedArr.length; j++) {
+          starNames.add(modifiedArr[j]);
+        }
+      }
+      this.stars.add(new Star(xCord, yCord, henryDraper, magnitude, starNames));
+    }
+    populateMaps();
+  }
+
+  void populateMaps() {
+    for (int i = 0; i < this.stars.size(); i++) {
+      this.identityToLocation.put(
+          this.stars.get(i).getHenryDraper(),
+          new Point(this.stars.get(i).getXCord(), this.stars.get(i).getYCord())
+        );
+      this.namesToIdentity.put(
+          this.stars.get(i).getNames(),
+          this.stars.get(i).getHenryDraper()
+        );
+    }
+  }
+
+  public Point coordsToPixel(double origX, double origY, int size) {
+    double newX = (origX + 1.0) * (size / 2.0);
+    double newY = (origY + 1.0) * (size / 2.0);
+    return new Point(newX, newY);
+  }
+
+  void drawStars() {
+    StdDraw.setPenColor(Color.BLACK);
+    StdDraw.filledSquare(SIZE / 2, SIZE / 2, SIZE / 2);
+    StdDraw.setPenColor(Color.WHITE);
+    for (int i = 0; i < this.stars.size(); i++) {
+      double radius = Math.round(10.0 / (this.stars.get(i).getMagnitude() + 2));
+      double newX =
+        this.coordsToPixel(
+            this.stars.get(i).getXCord(),
+            this.stars.get(i).getYCord(),
+            SIZE
+          )
+          .getX();
+      double newY =
+        this.coordsToPixel(
+            this.stars.get(i).getXCord(),
+            this.stars.get(i).getYCord(),
+            SIZE
+          )
+          .getY();
+      StdDraw.filledCircle(newX, newY, radius);
+    }
+  }
+
+  void drawConstellations() throws FileNotFoundException {
+    StdDraw.setPenColor(Color.YELLOW);
+    File[] files = {
+      new File(
+        "C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/BigDipper.txt"
+      ),
+      new File(
+        "C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/Bootes.txt"
+      ),
+      new File(
+        "C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/Cassiopeia.txt"
+      ),
+      new File(
+        "C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/Cygnet.txt"
+      ),
+      new File(
+        "C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/Gemini.txt"
+      ),
+      new File(
+        "C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/Hydra.txt"
+      ),
+      new File(
+        "C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/UrsaMajor.txt"
+      ),
+      new File(
+        "C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/UrsaMinor.txt"
+      ),
+    };
+    for (File f : files) {
+      Scanner file = new Scanner(f);
+      double sourceX = 0.0;
+      double sourceY = 0.0;
+      double destinationX = 0.0;
+      double destinationY = 0.0;
+      while (file.hasNextLine()) {
+        String[] curr = file.nextLine().split(",");
+        String starOne = curr[0];
+        String starTwo = curr[1];
+        for (int i = 0; i < curr.length; i++) {
+          for (int j = 0; j < stars.size(); j++) {
+            if (stars.get(j).getNames().size() >= 1) {
+              ArrayList<String> names = stars.get(j).getNames();
+              for (int k = 0; k < names.size(); k++) {
+                if (names.get(k).equals(starOne)) {
+                  sourceX =
+                    identityToLocation.get(namesToIdentity.get(names)).getX();
+                  sourceY =
+                    identityToLocation.get(namesToIdentity.get(names)).getY();
+                } else if (names.get(k).equals(starTwo)) {
+                  destinationX =
+                    identityToLocation.get(namesToIdentity.get(names)).getX();
+                  destinationY =
+                    identityToLocation.get(namesToIdentity.get(names)).getY();
                 }
-                for (int j = 0; j < modifiedArr.length; j++){
-                    starNames.add(modifiedArr[j]);
-                }
+              }
             }
-            this.stars.add(new Star(xCord, yCord, henryDraper, magnitude, starNames));
+          }
         }
-        // populateMaps();
+        StdDraw.line(sourceX, sourceY, destinationX, destinationY);
+      }
     }
-
-    void populateMaps(){
-        for (int i = 0; i < this.stars.size(); i++){
-            this.identityToLocation.put(this.stars.get(i).getHenryDraper(), new Point(this.stars.get(i).getXCord(), this.stars.get(i).getYCord()));
-            this.namesToIdentity.put(this.stars.get(i).getNames(), this.stars.get(i).getHenryDraper());
-        }
-        System.out.println(namesToIdentity);
-    }
-
-    public Point coordsToPixel(double origX, double origY, int size){
-       double newX = (origX + 1.0) * (size / 2.0);
-       double newY = (origY + 1.0) * (size / 2.0);
-       return new Point(newX, newY);
-    }
-
-    void drawStars(){
-        StdDraw.setPenColor(Color.BLACK);
-        StdDraw.filledSquare(256, 256, 256);
-        StdDraw.setPenColor(Color.WHITE);
-        for (int i = 0; i < this.stars.size(); i++){
-            double radius = Math.round(10.0 / (this.stars.get(i).getMagnitude() + 2));
-            double newX = this.coordsToPixel(this.stars.get(i).getXCord(), this.stars.get(i).getYCord(), 512).getX();
-            double newY = this.coordsToPixel(this.stars.get(i).getXCord(), this.stars.get(i).getYCord(), 512).getY();
-            StdDraw.filledCircle(newX, newY, radius);
-        }
-    }
-
-    void drawConstellation(String fileName) throws FileNotFoundException{
-        StdDraw.setPenColor(Color.YELLOW);
-        Scanner file = new Scanner(new File(fileName));
-        while (file.hasNextLine()){
-            String[] curr = file.nextLine().split(",");
-            // get location of 1st star and draw a line from it to 2nd star
-            // double firstX = 0.0; double firstY = 0.0; double secondX = 0.0; double secondY = 0.0;
-            // for (int i = 0; i < stars.size(); i++){
-            //     ArrayList<String> n = stars.get(i).getNames();
-            //     if (n.size() > 0 && n.size() == 1){
-            //         if (n.get(0).equals(curr[0])){
-            //             firstX = stars.get(i).getXCord();
-            //             firstY = stars.get(i).getYCord();
-            //         }
-            //         if (n.get(0).equals(curr[1])){
-            //             secondX = stars.get(i).getXCord();
-            //             secondY = stars.get(i).getYCord();
-            //         }
-            //     }
-            // }
-            // StdDraw.line(firstX, firstY, secondX, secondY);
-        }
-    }
+  }
 }
--- Constellations/StarChartRunner.java
+++ Constellations/StarChartRunner.java
@@ -1,15 +1,9 @@
 import java.io.FileNotFoundException;
 
 public class StarChartRunner {
-
-    static final int SIZE = 512;
-
     public static void main(String[] args) throws FileNotFoundException{
-        StdDraw.setCanvasSize(SIZE, SIZE);
-        StdDraw.setXscale(0, SIZE);
-        StdDraw.setYscale(0, SIZE); 
         StarChart chart = new StarChart("C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/stars.txt");
         chart.drawStars();
-        chart.drawConstellation("C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/Constellations/Constellation Files/BigDipper.txt");
+        chart.drawConstellations();
     }
 }
