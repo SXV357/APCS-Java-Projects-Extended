ad82e96467d0a86ff928c137cec7a4ff763fdc47
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-06-05 23:19:44 -0500

    configure correct file path for USA.txt

--- PurpleAmerica/Runner.java
+++ PurpleAmerica/Runner.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Runner {
     public static void main(String[] args) throws FileNotFoundException{
-        White map = new White("C:/Users/14058/OneDrive/Desktop/APCS-Projects-Extended/PurpleAmerica/USA.txt");
+        White map = new White("/Users/shreyasviswanathan/Desktop/Programming/Java Projects/APCS-Java-Projects-Extended/PurpleAmerica/USA.txt");
     }
 }
+
